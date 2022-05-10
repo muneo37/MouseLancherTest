9292e412a7e3b4c7357f4a84924220f105bf08ed
muneo37 <y.wai.ysuke0822@gmail.com>
2022-05-10 11:11:20 +0900

    MouseLancherWindowを追加

--- MouseLancherTest/MainWindow.xaml.cs
+++ MouseLancherTest/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace MouseLancherTest
 {
@@ -23,6 +10,9 @@ namespace MouseLancherTest
         public MainWindow()
         {
             InitializeComponent();
+
+            var lancherWindow = new MouseLancherWindow();
+            lancherWindow.Show();
         }
     }
 }
